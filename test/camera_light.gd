4bee9d0e6b44d72b2e2aec69ab7deec97d7c24e3
hamonstudio <67489617+hamonstudio@users.noreply.github.com>
2025-07-20 05:15:57 +0200

    Deleting Wwise

--- test/camera_light.gd
+++
@@ -1,6 +0,0 @@
-extends Node3D
-
-@export var player: Node3D
-
-func _process(_delta):
-	position = player.position
--- test/camera_spring_arm.gd
+++
@@ -1,6 +0,0 @@
-extends SpringArm3D
-
-@export var player: Node3D
-
-func _process(_delta):
-	position = player.position
