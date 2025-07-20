4d2d9de89015a0443673b8b9a0f5d9267b01b377
hamonstudio <67489617+hamonstudio@users.noreply.github.com>
2025-07-20 23:56:32 +0200

    Cleaned up player code, added vfx on projectile collision

--- test/script/projectile_laucher.gd
+++ test/script/projectile_laucher.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 const PROJECTILE = preload("res://test/scenes/projectile.tscn")
 
-@export var cooldown := 1.0
+@export var cooldown := 0.75
 
 @onready var timer: Timer = $Timer
 
@@ -11,15 +11,16 @@ var trigger_held = false
 func _physics_process(delta: float) -> void:
 	if timer.is_stopped() and trigger_held:
 		timer.start(cooldown)
-		var attack = PROJECTILE.instantiate()
-		add_child(attack)
-		attack.global_transform = global_transform
+		var projectile_child = PROJECTILE.instantiate()
+		add_child(projectile_child)
+		projectile_child.global_transform = global_transform
 
 
 func _on_player_s_shoot() -> void:
 	trigger_held = true
 	#print("trigger held")
-	
+
+
 func _on_player_s_shoot_stop() -> void:
 	trigger_held = false
 	#print("trigger released")
--- test/script/projectile_raycats.gd
+++ test/script/projectile_raycats.gd
@@ -1,6 +1,9 @@
 extends RayCast3D
 
+const IMPACT = preload("res://vfx/vfx_explosion_small.tscn")
+
 @export var projectile_speed := 60.0
+@export var impact_duration := 1.0
 
 @onready var remote_transform := RemoteTransform3D.new()
 
@@ -13,9 +16,15 @@ func _physics_process(delta: float) -> void:
 	if is_colliding():
 		global_position = get_collision_point()
 		set_physics_process(false)
+		var impact_child = IMPACT.instantiate()
+		collider.add_child(impact_child)
 		collider.add_child(remote_transform)
+		
+		impact_child.global_transform = global_transform
 		remote_transform.global_transform = global_transform
 		remote_transform.remote_path = remote_transform.get_path_to(self)
+		
+		await get_tree().create_timer(impact_duration).timeout
 		remote_transform.tree_exited.connect(cleanup)
 
 func cleanup() -> void:
